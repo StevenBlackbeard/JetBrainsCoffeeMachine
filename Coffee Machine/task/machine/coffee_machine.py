478d63ce872ca259d9b54f6ee72f18bea808eb32
Steven Blackbeard <steven.blackbeard@hotmail.co.uk>
2022-02-14 13:21:38 +0000

    latest commit

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,74 +1,22 @@
-class Coffee:
-    def __init__(self):
-        self.action = None
-        self.action_buy = None
-        self.supplies = {
-            'water': 400,
-            'milk': 540,
-            'coffee beans': 120,
-            'disposable cups': 9,
-            'money': 550
-        }
-        self.amounts = {1: {"water": -250, "milk": -0, "coffee beans": -16, "disposable cups": -1, "money": 4},
-                        2: {"water": -350, "milk": -75, "coffee beans": -20, "disposable cups": -1, "money": 7},
-                        3: {"water": -200, "milk": -100, "coffee beans": -12, "disposable cups": -1, "money": 6}}
+import math
 
-    def remaining(self):
-        print(f'''
-The coffee machine has:
-{self.supplies["water"]} ml of water
-{self.supplies["milk"]} ml of milk
-{self.supplies["coffee beans"]} g of coffee beans
-{self.supplies["disposable cups"]} disposable cups
-${self.supplies["money"]} of money
-        ''')
-
-    def fill(self):
-        self.supplies["water"] += int(input("Write how many ml of water you want to add:"))
-        self.supplies["milk"] += int(input("Write how many ml of milk you want to add:"))
-        self.supplies["coffee beans"] += int(input("Write how many grams of coffee beans you want to add:"))
-        self.supplies["disposable cups"] += int(input("Write how many disposable cups of coffee you want to add:"))
-
-    def take(self):
-        print(f'I gave you ${self.supplies["money"]}')
-        self.supplies["money"] = 0
-
-    def check(self):
-        check = {}
-        for key in [key for key in self.supplies.keys() if key not in "money"]:
-            check[key] = self.supplies[key] > -self.amounts[self.action_buy][key]
-        return check
-
-    def buy(self):
-        self.action_buy = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-        if self.action_buy != "back":
-            self.action_buy = int(self.action_buy)
-            check_vals = self.check()
-            if all(check_vals.values()):
-                print("I have enough resources, making you a coffee!")
-                for key, value in self.amounts[self.action_buy].items():
-                    self.supplies[key] += value
-            else:
-                not_enough = "".join([key for key, value in check_vals.items() if value == False])
-                print(f'Sorry, not enough {not_enough}!')
-
-    def take_action(self):
-        while True:
-            self.action = input("Write action (buy, fill, take, remaining, exit):")
-            print()
-            if self.action == "take":
-                self.take()
-                continue
-            if self.action == "remaining":
-                self.remaining()
-                continue
-            if self.action == "buy":
-                self.buy()
-                continue
-            elif self.action == "fill":
-                self.fill()
-            else:
-                break
-
-test = Coffee()
-test.take_action()
+avail_water = int(input("Write how many ml of water the coffee machine has:"))
+avail_milk = int(input("Write how many ml of milk the coffee machine has:"))
+avail_beans = int(input("Write how many grams of coffee beans the coffee machine has:"))
+cups = int(input("Write how many cups of coffee you will need:"))
+# water = cups * 200
+# milk = cups * 50
+# beans = cups * 15
+avail_cups = min(
+    math.floor(avail_water / 200),
+    math.floor(avail_milk / 50),
+    math.floor(avail_beans / 15)
+)
+if cups == avail_cups:
+    print("Yes, I can make that amount of coffee")
+elif cups > avail_cups:
+    print(f'No, I can make only {avail_cups} cups of coffee')
+else:
+    print(f'Yes, I can make that amount of coffee (and even {avail_cups - cups} more than that)')
+# print(f'For {cups} cups of coffee you will need:')
+# print(f'{water} ml of water\n{milk} ml of milk\n{beans} g of coffee beans')
