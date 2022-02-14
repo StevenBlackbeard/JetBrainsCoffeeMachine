79068cb292847a23516a1a6c1a9f39b02e7d77e2
Steven Blackbeard <steven.blackbeard@hotmail.co.uk>
2022-02-14 13:10:45 +0000

    latest commit

---
+++ Topics/How to read the documentation/Math from NumPy/main.py
@@ -0,0 +1,6 @@
+import math
+
+def calculate_cosine(angle_in_degrees):
+    # do not forget to round the result and print it
+    val_in = angle_in_degrees
+    print(round(math.cos(math.radians(val_in)), 2))
